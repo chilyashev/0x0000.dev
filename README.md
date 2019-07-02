0eab113afd50d0b9fa85588db63e590f5c7e8716
Mihail Chilyashev <mihail.chilyashev@parkingtelecom.com>
2019-07-02 16:37:44 +0300

    Added Nuxt.js

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # 0x0000.dev
-Personal website ([https://0x0000.dev](https://0x0000.dev))
+
+> A personal website
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm run install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app dark>
+    <nuxt/>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        clipped: false,
+        drawer: false,
+        fixed: false,
+        items: [],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'Access violation at 0x00000000'
+      }
+    },
+    head() {
+      return {
+        title: this.title,
+        meta: [
+          // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+          {hid: 'description', name: 'description', content: 'A personal website'}
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
