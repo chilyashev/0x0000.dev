699fc2b0f3c2c62259d2a6f15166b0fd3ab72fd1
Mihail Chilyashev <chilyashev@gmail.com>
2019-07-14 14:54:22 +0300

    Started working on a better front page

--- pages/about.vue
+++ pages/about.vue
@@ -1,20 +1,28 @@
 <!--suppress HtmlUnknownTag -->
 <template>
-  <div>
-    Lorem ipsum
-  </div>
+    <v-content>
+        <v-container>
+            <h1>About</h1>
+        </v-container>
+        <v-container>
+            <p>Hi there,</p>
+
+            <p>My name is Mihail Chilyashev and I spend most of my days writing software and solving the problems this
+                creates.</p>
+            <p></p>
+            <p>You can find my CV here: <a href="https://chilyashev.com/cv" target="_blank">https://chilyashev.com/cv.</a></p>
+        </v-container>
+    </v-content>
 </template>
 
 <script>
 
-  export default {
-    components: {},
-    data() {
-      return {
-        host: 'kernelshocked'
-      }
+    export default {
+        components: {},
+        data() {
+            return {}
+        }
     }
-  }
 </script>
 
 
